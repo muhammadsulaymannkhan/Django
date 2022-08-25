b1318e50d49314ab2d8392443e60e953c95e4617
muhammadsulaymannkhan <muhammadsulaymannkhan@gmail.com>
2022-08-25 21:50:10 +0500

    connected to postgres

--- tasks/tasks/settings.py
+++ tasks/tasks/settings.py
@@ -96,8 +96,19 @@ WSGI_APPLICATION = 'tasks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'postgres',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'woslucra',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
 }
 
